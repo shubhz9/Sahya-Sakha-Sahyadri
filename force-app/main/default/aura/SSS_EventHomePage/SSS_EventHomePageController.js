854251b18870e673de5a11798ca06edd58fd3a3c
shubhz9 <shubhamrajendraraut@gmail.com>
2023-04-01 19:00:55 +0530

    Added Guest User site changes.

--- force-app/main/default/aura/SSS_EventHomePage/SSS_EventHomePageController.js
+++ force-app/main/default/aura/SSS_EventHomePage/SSS_EventHomePageController.js
@@ -10,5 +10,16 @@
     submitDetails : function(component, event, helper) {
 		var flow = component.find("flowData");
     	flow.startFlow("New_Event");
+	},
+    fetchEventDetails : function(component, event, helper) {
+		var action = component.get("c.fetchEventDetailsAura");
+        action.setCallback(this, function(response) {
+            var state = response.getState();
+            console.log('state: ',state);
+            if (state === "SUCCESS") {
+                
+            }
+        });
+        $A.enqueueAction(action);
 	}
 })
